64ac145c08cee0c4d2393a53e75dc0559136cd97
Souzatitan <asouzanderson@gmail.com>
2024-12-10 23:01:42 -0300

    inivcio

---
+++ assets/js/questions.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Voce se define como uma pessoa ?",
+        answer:"a) document.querySelector()",
+        options: [
+            "a) Corajosa e leal",
+            "b) Determinado e altruísta",
+            "c) Sensível e criativo",
+            "d) Engraçado e carismático"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"O que acontece quando você usa event.preventDefault() em um evento?",
+        answer:"b) O comportamento padrão do evento é interrompido.",
+        options: [
+            "a) A página é recarregada automaticamente.",
+            "b) O comportamento padrão do evento é interrompido.",
+            "c) O evento é cancelado sem acionar a função associada.",
+            "d) A execução da função para imediatamente."
+        ]
+    },
+    {
+        numb: 3,
+        question:"Qual método JavaScript é usado para adicionar um evento a um elemento HTML?",
+        answer:"c) element.addEventListener()",
+        options: [
+            "a) element.addEvent()",
+            "b) element.attachEvent()",
+            "c) element.addEventListener()",
+            "d) element.setEvent()"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Qual das opções abaixo é usada para transformar uma NodeList em um array em JavaScript?",
+        answer:"b) Array.from()",
+        options: [
+            "a) Array.nodeList()",
+            "b) Array.from()",
+            "c) nodeList.toArray()",
+            "d) NodeList.map()"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+     {
+        numb: 6,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+     {
+        numb: 7,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+     {
+        numb: 8,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+     {
+        numb: 9,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+     {
+        numb: 10,
+        question:"Qual declaração em JavaScript pode ser usada para criar uma variável que não pode ser reatribuída?",
+        answer:"c) const",
+        options: [
+            "a) let",
+            "b) var",
+            "c) const",
+            "d) function"
+        ]
+    },
+
+];
